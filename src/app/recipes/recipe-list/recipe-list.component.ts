172cd9eb1bf6956518ed417caec9beb67184ba2d
Jkapanadze <jakobskay@gmail.com>
2021-11-10 00:15:30 +0400

    basic markup

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[]= [
+    new Recipe('Khinkali', 'Precious Georgian food',
+      'https://i.pinimg.com/originals/9a/96/d1/9a96d1e92f7c3321a5c1de8c21524ba8.jpg'),
+    new Recipe('Mtsvadi', 'Delicious food from Georgia',
+      'https://gastronomia.ge/wp-content/uploads/2020/06/00-1.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
