a8540b84542802aae450e2c823d597c89f9de9c6
Jkapanadze <jakobskay@gmail.com>
2021-12-04 16:45:14 +0400

    replaced eventemiters with subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Recipes';
-  loadedFeature = 'recipes';
-
-  onNavigate(loadedFeature){
-    this.loadedFeature = loadedFeature;
-  }
 }
